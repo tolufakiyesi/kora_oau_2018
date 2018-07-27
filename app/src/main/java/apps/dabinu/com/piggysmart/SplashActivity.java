0935800df5fb06a49eeb8c2803f4b9488f24a6ba
TaslimOseni <tasleemoseni@gmail.com>
2018-07-27 16:19:57 +0100

    Added dependencies and created landing page mock

--- app/src/main/java/apps/dabinu/com/piggysmart/SplashActivity.java
+++ app/src/main/java/apps/dabinu/com/piggysmart/SplashActivity.java
@@ -1,5 +1,7 @@
 package apps.dabinu.com.piggysmart;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new CountDownTimer(3000, 1000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+            }
+        }.start();
     }
 }
