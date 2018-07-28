219292fb6f73476f52360fae01e8f12fda81cdb9
TaslimOseni <tasleemoseni@gmail.com>
2018-07-28 08:46:59 +0100

    Done with login and signup

--- app/src/main/java/apps/dabinu/com/piggysmart/activities/LoginActivity.java
+++ app/src/main/java/apps/dabinu/com/piggysmart/activities/LoginActivity.java
@@ -1,7 +1,11 @@
 package apps.dabinu.com.piggysmart.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import apps.dabinu.com.piggysmart.R;
 
@@ -11,5 +15,37 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.submitCard).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(((EditText) findViewById(R.id.email)).getText().toString().trim().equals("")){
+                    ((EditText) findViewById(R.id.email)).setError("This field is required");
+                    Toast.makeText(getApplicationContext(), "THis field is required", Toast.LENGTH_LONG).show();
+                }
+                else if(((EditText) findViewById(R.id.password)).getText().toString().trim().equals("")){
+                    ((EditText) findViewById(R.id.password)).setError("This field is required");
+                    Toast.makeText(getApplicationContext(), "THis field is required", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
+                    //log in with Firebase
+                }
+            }
+        });
+
+
+        findViewById(R.id.signup).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), SignupActivity.class));
+            }
+        });
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
     }
 }
--- app/src/main/java/apps/dabinu/com/piggysmart/activities/SignupActivity.java
+++ app/src/main/java/apps/dabinu/com/piggysmart/activities/SignupActivity.java
@@ -1,7 +1,11 @@
 package apps.dabinu.com.piggysmart.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import apps.dabinu.com.piggysmart.R;
 
@@ -11,5 +15,44 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
+
+
+        findViewById(R.id.signup).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(((EditText) findViewById(R.id.usernameText)).getText().toString().trim().equals("")){
+                    ((EditText) findViewById(R.id.usernameText)).setError("This field is required");
+                    Toast.makeText(getApplicationContext(), "THis field is required", Toast.LENGTH_LONG).show();
+                }
+                else if(((EditText) findViewById(R.id.emailText)).getText().toString().trim().equals("")){
+                    ((EditText) findViewById(R.id.emailText)).setError("This field is required");
+                    Toast.makeText(getApplicationContext(), "THis field is required", Toast.LENGTH_LONG).show();
+                }
+                else if(((EditText) findViewById(R.id.phoneNumberText)).getText().toString().trim().equals("")){
+                    ((EditText) findViewById(R.id.phoneNumberText)).setError("This field is required");
+                    Toast.makeText(getApplicationContext(), "THis field is required", Toast.LENGTH_LONG).show();
+                }
+                else if(((EditText) findViewById(R.id.passwordText)).getText().toString().trim().equals("")){
+                    ((EditText) findViewById(R.id.passwordText)).setError("This field is required");
+                    Toast.makeText(getApplicationContext(), "THis field is required", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
+                    //sign up with Firebase
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+
     }
 }
