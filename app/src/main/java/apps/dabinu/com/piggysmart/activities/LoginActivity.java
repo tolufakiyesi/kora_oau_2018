68d427307a2e6dd7964f516ca8b2fe14444fa6ab
TaslimOseni <tasleemoseni@gmail.com>
2018-07-28 03:58:48 +0100

    Log-in and sign-up activities created.

---
+++ app/src/main/java/apps/dabinu/com/piggysmart/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package apps.dabinu.com.piggysmart.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import apps.dabinu.com.piggysmart.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
